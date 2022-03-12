48fa31ff62ecb6f8e03abd8d3166c062f0947925
Cyber-Mitch <osawarumitchell@gmail.com>
2022-03-12 23:19:58 +0100

    Hello World.sol

---
+++ Hello World.sol
@@ -0,0 +1,18 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract HelloWorld{
+    string public Greetings;
+
+    constructor (){
+        Greetings = "Hello World";
+    }
+
+    function add(uint x, uint y) external pure returns (uint){
+        return x + y;
+    }
+
+    function sub(uint x, uint y) external pure returns (uint) {
+        return x - y;
+    }
+}
